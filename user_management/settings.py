a0daf2955bee5bfb81d188b05e62c2d4c787cd84
Vivek Parmar <58722335+V-Flash@users.noreply.github.com>
2024-10-28 18:49:07 +0530

    gse

--- user_management/settings.py
+++ user_management/settings.py
@@ -136,10 +136,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
--- user_management/urls.py
+++ user_management/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include, re_path
 
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
 from django.contrib.auth import views as auth_views
 from users.views import CustomLoginView, ResetPasswordView, ChangePasswordView
 
@@ -34,4 +34,4 @@ urlpatterns = [
 
     re_path(r'^oauth/', include('social_django.urls', namespace='social')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] +  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
